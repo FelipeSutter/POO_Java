6aef3d0b86879bfbc20465faf1d4c79c14fc4787
Felipe Sutter <lipeps61@gmail.com>
2023-08-26 15:10:51 -0300

    Lista 1 - Exercício 2

--- listas_lp/src/br/com/poo/lista1/Exercicio2.java
+++ listas_lp/src/br/com/poo/lista1/Exercicio2.java
@@ -1,10 +1,24 @@
 package br.com.poo.lista1;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int pessoa;
+		int soma = 0;
+		for(int i = 0; i < 5; i++) {
+			System.out.printf("Qual é a idade da pessoa %d? ", i+1);
+			pessoa = sc.nextInt();
+			soma += pessoa;
+			if(i == 4) {
+				System.out.printf("Qual é a minha idade? ");
+				pessoa = sc.nextInt();
+				soma += pessoa;
+			}
+		}
+		System.out.printf("A soma das pessoas é: %d", soma);
 	}
 
 }
