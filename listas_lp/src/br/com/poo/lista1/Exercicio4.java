679a15ae7ea28f93a6a39db7225fa66a5cd5237e
Felipe Sutter <lipeps61@gmail.com>
2023-08-27 16:10:29 -0300

    Lista 1 - Exercício 4

--- listas_lp/src/br/com/poo/lista1/Exercicio4.java
+++ listas_lp/src/br/com/poo/lista1/Exercicio4.java
@@ -1,10 +1,18 @@
 package br.com.poo.lista1;
 
+import java.util.Scanner;
+
 public class Exercicio4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		double C, F;
+		System.out.print("Digite a temperatura desejada: ");
+		C = sc.nextInt();
+		F = C * 1.8 + 32;
+		System.out.printf("Temperatura em C: %.0f\n", C);
+		System.out.printf("Temperatura em F: %.0f", F);
+		sc.close();
 	}
 
 }
