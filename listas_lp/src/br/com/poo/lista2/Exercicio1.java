c6998b14c74657e14e82cb934b1d9efd11c4bfdd
Felipe Sutter <lipeps61@gmail.com>
2023-08-28 12:26:04 -0300

    Lista 2 - Exercício 1

---
+++ listas_lp/src/br/com/poo/lista2/Exercicio1.java
@@ -0,0 +1,25 @@
+package br.com.poo.lista2;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int UNIVERSO = 32;
+		boolean verdadeiro = true;
+		String letra;
+		while(verdadeiro) {
+			System.out.print("Qual o significado da vida, do universo e tudo mais? Digite S para saber: ");
+			letra = sc.next();
+			if(letra.equals("s") || letra.equals("S")) {
+				System.out.println(UNIVERSO);
+			}
+			else {
+				System.out.println("Que sem graca!");
+				verdadeiro = false;
+			}
+		}
+	}
+
+}
