8af05333b69afef813f08ddf020ce4dd62ec7bdc
Felipe Sutter <lipeps61@gmail.com>
2023-08-28 17:56:50 -0300

    Adição do sleep

--- listas_lp/src/br/com/poo/lista2/Exercicio3.java
+++ listas_lp/src/br/com/poo/lista2/Exercicio3.java
@@ -28,6 +28,11 @@ public class Exercicio3 {
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < i; j++) {
 				System.out.print(".");
+				try {
+					  Thread.sleep(500);
+				} catch (InterruptedException e) {
+					  Thread.currentThread().interrupt();
+					}
 			}
 		}
 	}
