78123d8c78fa997c079000164aa100226aaa07a3
Felipe Sutter <lipeps61@gmail.com>
2023-09-03 13:43:19 -0300

    Classe main da conta Banco

---
+++ Listas_LP_Java/src/br/com/poo/bancoExemplo/MainBanco.java
@@ -0,0 +1,13 @@
+package br.com.poo.bancoExemplo;
+
+public class MainBanco {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ContaBanco cb = new ContaBanco(100,"Felipe",200.00);
+		
+		System.out.println(cb);
+
+	}
+
+}
