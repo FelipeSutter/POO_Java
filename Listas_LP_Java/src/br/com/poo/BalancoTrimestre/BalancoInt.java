db69c87862305c58d3cad67c9de9c54679994ab6
Felipe Sutter <lipeps61@gmail.com>
2023-09-03 13:35:41 -0300

    Adição dos loggers e variáveis de teste

--- Listas_LP_Java/src/br/com/poo/BalancoTrimestre/BalancoInt.java
+++ Listas_LP_Java/src/br/com/poo/BalancoTrimestre/BalancoInt.java
@@ -12,28 +12,38 @@ public class BalancoInt {
 	private static Logger logger = Logger.getLogger(BalancoInt.class.getName());
 	public int num = 1000;
 	protected int numEx = 2000;
+
+	public Integer getGastoJaneiro() {
+		return gastoJaneiro;
+	}
 	
 	public int soma() {
-		//System.out.printf("A soma total do trimestre eh: %d\n", somaTotal);
-		logger.log(Level.INFO, "A soma total do trimestre eh: " + somaTotal);
-		// olhar o sonarLint no logger e achar uma solução do pq o sublinhado
+		String mensagem = String.format("A soma total do trimestre eh: %d", somaTotal);
+		logger.log(Level.INFO,mensagem);
+		logger.log(Level.INFO,"A soma total do trimestre eh: {0}\n", somaTotal);
 		return somaTotal;
 	}
 	
 	public int soma(int janeiro) {
-		System.out.printf("A soma de janeiro eh: %d\n", janeiro);
+		String mensagem = String.format("A soma de janeiro eh: %d", janeiro);
+		logger.log(Level.INFO,mensagem);
+		logger.log(Level.INFO,"A soma de janeiro eh: {0}\n", janeiro);
 		return janeiro;
 	}
 	
 	public int soma(int janeiro, int fevereiro) {
-		int somaTotal = janeiro + fevereiro;
-		System.out.printf("A soma do bimestre eh: %d\n", somaTotal);
-		return somaTotal;
+		int somaTotal1 = janeiro + fevereiro;
+		String mensagem = String.format("A soma do bimestre eh: %d", somaTotal1);
+		logger.log(Level.INFO,mensagem);
+		logger.log(Level.INFO,"A soma do bimestre eh: {0}\n", somaTotal1);
+		return somaTotal1;
 	}
 	
 	public int soma(int janeiro, int fevereiro, int marco) {
-		int somaTotal = janeiro + fevereiro + marco;
-		System.out.printf("A soma do trimestre eh: %d\n", somaTotal);
-		return somaTotal;
+		int somaTotal2 = janeiro + fevereiro + marco;
+		String mensagem = String.format("A soma do trimestre eh: %d", somaTotal2);
+		logger.log(Level.INFO,mensagem);
+		logger.log(Level.INFO,"A soma do trimestre eh: {0}\n", somaTotal2);
+		return somaTotal2;
 	}
 }
