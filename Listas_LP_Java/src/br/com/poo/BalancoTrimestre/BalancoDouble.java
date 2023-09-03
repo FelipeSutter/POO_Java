a78090c203056877572aecdaaad62eb913c25e8b
Felipe Sutter <lipeps61@gmail.com>
2023-09-03 13:40:31 -0300

    Loggers na classe BalancoDouble

--- Listas_LP_Java/src/br/com/poo/BalancoTrimestre/BalancoDouble.java
+++ Listas_LP_Java/src/br/com/poo/BalancoTrimestre/BalancoDouble.java
@@ -14,7 +14,7 @@ public class BalancoDouble {
 	public double soma() {
 		String mensagem = String.format("A soma total do trimestre eh: %.2f", somaTotal);
 		logger.log(Level.INFO,mensagem);
-		logger.log(Level.INFO,"A soma total do trimestre eh: {0}", somaTotal);
+		logger.log(Level.INFO,"A soma total do trimestre eh: {0}\n", somaTotal);
 		// {0} age como se fosse o % do printf, e se quiser botar mais variáveis vai aumentando
 		return somaTotal;
 	}
@@ -22,26 +22,26 @@ public class BalancoDouble {
 	public double soma(double janeiro) {
 		String mensagem = String.format("A soma de janeiro eh: %.2f", janeiro);
 		logger.log(Level.INFO,mensagem);
-		logger.log(Level.INFO,"A soma de janeiro eh: {0}", janeiro);
+		logger.log(Level.INFO,"A soma de janeiro eh: {0}\n", janeiro);
 		// {0} age como se fosse o % do printf, e se quiser botar mais variáveis vai aumentando
 		return janeiro;
 	}
 	
 	public double soma(double janeiro, double fevereiro) {
-		double somaTotal = janeiro + fevereiro;
-		String mensagem = String.format("A soma do bimestre eh: %.2f", somaTotal);
+		double somaTotal1 = janeiro + fevereiro;
+		String mensagem = String.format("A soma do bimestre eh: %.2f", somaTotal1);
 		logger.log(Level.INFO,mensagem);
-		logger.log(Level.INFO,"A soma do bimestre eh: {0}", somaTotal);
+		logger.log(Level.INFO,"A soma do bimestre eh: {0}\n", somaTotal1);
 		// {0} age como se fosse o % do printf, e se quiser botar mais variáveis vai aumentando
-		return somaTotal;
+		return somaTotal1;
 	}
 	
 	public double soma(double janeiro, double fevereiro, double marco) {
-		double somaTotal = janeiro + fevereiro + marco;
-		String mensagem = String.format("A soma do trimestre eh: %.2f", somaTotal);
+		double somaTotal2 = janeiro + fevereiro + marco;
+		String mensagem = String.format("A soma do trimestre eh: %.2f", somaTotal2);
 		logger.log(Level.INFO,mensagem);
-		logger.log(Level.INFO,"A soma do trimestre eh: {0}", somaTotal);
-		return somaTotal;
+		logger.log(Level.INFO,"A soma do trimestre eh: {0}\n", somaTotal2);
+		return somaTotal2;
 	}
 	
 }
