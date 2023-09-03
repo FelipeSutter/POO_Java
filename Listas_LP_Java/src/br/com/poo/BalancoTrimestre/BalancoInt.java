e00360327d32b96b178ea6a1c9ef58f3e9c92150
Felipe Sutter <lipeps61@gmail.com>
2023-09-03 14:40:35 -0300

    Adição de construtores e toString

--- Listas_LP_Java/src/br/com/poo/BalancoTrimestre/BalancoInt.java
+++ Listas_LP_Java/src/br/com/poo/BalancoTrimestre/BalancoInt.java
@@ -8,13 +8,40 @@ public class BalancoInt {
 	private Integer gastoJaneiro = 15000;
 	private Integer gastoFevereiro = 23000;
 	private Integer gastoMarco = 17000;
+	private Integer gastoAbril = 10000;
+	private Integer gastoMaio = 8000;
+	private Integer gastoJunho = 20000;
 	private Integer somaTotal = gastoJaneiro + gastoFevereiro + gastoMarco;
 	private static Logger logger = Logger.getLogger(BalancoInt.class.getName());
 	public int num = 1000;
 	protected int numEx = 2000;
+	
+	public BalancoInt() {
+		
+	}
+
+	public BalancoInt(Integer gastoJaneiro, Integer gastoFevereiro, Integer gastoMarco) {
+		this.gastoJaneiro = gastoJaneiro;
+		this.gastoFevereiro = gastoFevereiro;
+		this.gastoMarco = gastoMarco;
+	}
+
+	public BalancoInt(Integer gastoJaneiro, Integer gastoFevereiro, Integer gastoMarco, Integer gastoAbril, Integer gastoMaio, Integer gastoJunho) {
+		this.gastoJaneiro = gastoJaneiro;
+		this.gastoFevereiro = gastoFevereiro;
+		this.gastoMarco = gastoMarco;
+		this.gastoAbril = gastoAbril;
+		this.gastoMaio = gastoMaio;
+		this.gastoJunho = gastoJunho;
+	}
 
 	public Integer getGastoJaneiro() {
 		return gastoJaneiro;
+	}
+	
+	@Override
+	public String toString() {
+		return "--- Balanço total ---\n\nJaneiro: \t" + gastoJaneiro + "\nFevereiro: \t" + gastoFevereiro + "\nMarço: \t\t" + gastoMarco + "\nAbril: \t\t" + gastoAbril + "\nMaio: \t\t" + gastoMaio + "\nJunho: \t\t" + gastoJunho + "\n---------------------";
 	}
 	
 	public int soma() {
