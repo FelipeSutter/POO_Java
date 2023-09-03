14373ae972bf076d6ebf51bd6f03067256ca0499
Felipe Sutter <lipeps61@gmail.com>
2023-09-03 13:36:10 -0300

    Classe que herda de BalancoInt para teste

---
+++ Listas_LP_Java/src/br/com/poo/BalancoTrimestre/BalancoTeste.java
@@ -0,0 +1,17 @@
+package br.com.poo.BalancoTrimestre;
+
+public class BalancoTeste extends BalancoInt {
+	
+	protected int varTeste = 10;
+	int var = 10;
+	
+	@Override
+	public int soma() {
+		System.out.println(numEx - varTeste);
+		return numEx - varTeste;
+	}
+	
+	public void banana() {
+		System.out.println(getGastoJaneiro());
+	}
+}
