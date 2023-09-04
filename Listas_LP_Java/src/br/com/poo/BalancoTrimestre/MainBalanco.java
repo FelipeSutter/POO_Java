52a30ab407cd522120d7a8bce08bc335dd9c3237
Felipe Sutter <lipeps61@gmail.com>
2023-09-04 13:15:35 -0300

    Classe main com as mudanças devidas

--- Listas_LP_Java/src/br/com/poo/BalancoTrimestre/MainBalanco.java
+++ Listas_LP_Java/src/br/com/poo/BalancoTrimestre/MainBalanco.java
@@ -1,28 +1,12 @@
 package br.com.poo.BalancoTrimestre;
 
+import java.math.BigDecimal;
+
 // import java.math.BigDecimal;
 
 public class MainBalanco {
 
 	public static void main(String[] args) {
-//		BalancoDouble bd = new BalancoDouble();
-//		BalancoBigD big = new BalancoBigD();
-//		bi.soma();
-//		bi.soma(100);
-//		bi.soma(100,200);
-//		bi.soma(100,200,300);
-//		System.out.println();
-//		
-//		bd.soma();
-//		bd.soma(10);
-//		bd.soma(100,200);
-//		bd.soma(0, 0, 0);
-//		System.out.println();
-//		
-//		big.soma();
-//		big.soma(new BigDecimal(200));
-//		big.soma(new BigDecimal(300),new BigDecimal(400));
-//		big.soma(new BigDecimal(600), new BigDecimal(800), new BigDecimal(1000));
 		
 		// declaração do construtor vazio
 		BalancoInt bi = new BalancoInt();
@@ -36,6 +20,26 @@ public class MainBalanco {
 		BalancoInt bisem = new BalancoInt(1000, 20009, 90123, 80764, 1234, 20001);
 		System.out.print(bisem); // alterou todos os valores
 		
+		BalancoDouble bd = new BalancoDouble();
+		BalancoBigD big = new BalancoBigD();
+		
+		bi.soma();
+		bi.soma(100);
+		bi.soma(100,200);
+		bi.soma(100,200,300);
+		System.out.println();
+		
+		bd.soma();
+		bd.soma(10);
+		bd.soma(100,200);
+		bd.soma(0, 0, 0);
+		System.out.println();
+		
+		big.soma();
+		big.soma(new BigDecimal(200));
+		big.soma(new BigDecimal(300),new BigDecimal(400));
+		big.soma(new BigDecimal(600), new BigDecimal(800), new BigDecimal(1000));
+				
 	}
 
 }
