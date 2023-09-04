b0fdf4ca41a18b42a8a7a36ead860ed2cd95396c
Felipe Sutter <lipeps61@gmail.com>
2023-09-04 15:09:21 -0300

    Classe pai de um exercício teste

---
+++ Listas_LP_Java/src/br/com/poo/ExerciciosTeste/ExCarro/Veiculo.java
@@ -0,0 +1,23 @@
+package br.com.poo.ExerciciosTeste.ExCarro;
+
+public class Veiculo { // super classe / classe pai
+    protected String marca;
+    protected String modelo;
+
+    public Veiculo(String marca, String modelo) { // construtor
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public void ligar() {
+        System.out.println("O veículo está ligado.");
+    }
+
+    public void desligar() {
+        System.out.println("O veículo está desligado.");
+    }
+
+    public void acelerar() {
+        System.out.println("O veículo está acelerando.");
+    }
+}
