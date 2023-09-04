93247f0bab5afed9d8490be8818e425cc5d583bc
Felipe Sutter <lipeps61@gmail.com>
2023-09-04 13:23:16 -0300

    Mudança para customLogger e formatação

--- Listas_LP_Java/src/br/com/poo/BalancoTrimestre/BalancoDouble.java
+++ Listas_LP_Java/src/br/com/poo/BalancoTrimestre/BalancoDouble.java
@@ -1,5 +1,6 @@
 package br.com.poo.BalancoTrimestre;
 
+import java.text.DecimalFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,38 +10,32 @@ public class BalancoDouble {
 	private Double gastoFevereiro = 23000.00;
 	private Double gastoMarco = 17000.00;
 	private Double somaTotal = gastoJaneiro + gastoFevereiro + gastoMarco;
-	private static Logger logger = Logger.getLogger(BalancoDouble.class.getName());
+	private Logger customLogger = Util.setupLogger();
+	private DecimalFormat df = new DecimalFormat("#,###.00");
 	
 	public double soma() {
-		String mensagem = String.format("A soma total do trimestre eh: %.2f", somaTotal);
-		logger.log(Level.INFO,mensagem);
-		logger.log(Level.INFO,"A soma total do trimestre eh: {0}\n", somaTotal);
-		// {0} age como se fosse o % do printf, e se quiser botar mais variáveis vai aumentando
+		Util.customizer();
+		customLogger.log(Level.INFO, () -> "\nO balanço trimestral é: " + df.format(somaTotal));
 		return somaTotal;
 	}
 	
 	public double soma(double janeiro) {
-		String mensagem = String.format("A soma de janeiro eh: %.2f", janeiro);
-		logger.log(Level.INFO,mensagem);
-		logger.log(Level.INFO,"A soma de janeiro eh: {0}\n", janeiro);
-		// {0} age como se fosse o % do printf, e se quiser botar mais variáveis vai aumentando
+		Util.customizer();
+		customLogger.log(Level.INFO, () -> "A soma de janeiro eh: " + df.format(janeiro));
 		return janeiro;
 	}
 	
 	public double soma(double janeiro, double fevereiro) {
 		double somaTotal1 = janeiro + fevereiro;
-		String mensagem = String.format("A soma do bimestre eh: %.2f", somaTotal1);
-		logger.log(Level.INFO,mensagem);
-		logger.log(Level.INFO,"A soma do bimestre eh: {0}\n", somaTotal1);
-		// {0} age como se fosse o % do printf, e se quiser botar mais variáveis vai aumentando
+		Util.customizer();
+		customLogger.log(Level.INFO, () -> "A soma do bimestre eh: " + df.format(somaTotal1));
 		return somaTotal1;
 	}
 	
 	public double soma(double janeiro, double fevereiro, double marco) {
 		double somaTotal2 = janeiro + fevereiro + marco;
-		String mensagem = String.format("A soma do trimestre eh: %.2f", somaTotal2);
-		logger.log(Level.INFO,mensagem);
-		logger.log(Level.INFO,"A soma do trimestre eh: {0}\n", somaTotal2);
+		Util.customizer();
+		customLogger.log(Level.INFO, () -> "A soma do trimestre eh: " + df.format(somaTotal2));
 		return somaTotal2;
 	}
 	
