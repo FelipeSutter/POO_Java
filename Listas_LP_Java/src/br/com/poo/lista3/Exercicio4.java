2f90b3d0b549e29c4cdae3e5d3b7ad5996749464
Felipe Sutter <lipeps61@gmail.com>
2023-09-05 11:25:43 -0300

    L3 - Ex4

---
+++ Listas_LP_Java/src/br/com/poo/lista3/Exercicio4.java
@@ -0,0 +1,25 @@
+package br.com.poo.lista3;
+
+import java.util.Random;
+
+import br.com.poo.BalancoTrimestre.Util;
+
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		int num;
+		int par = 0;
+		for(int i = 0; i < 20; i++) {
+			num = r.nextInt(1000);
+			Util.customizer();
+			System.out.println("Digite um número: " + num);
+			if(num % 2 == 0) {
+				par++;
+			}
+		}
+		System.out.println("O total de números pares digitados foi: " + par);
+
+	}
+
+}
