917fff6a1aa4d7a5779be4896a95856f61507768
Felipe Sutter <lipeps61@gmail.com>
2023-09-07 19:15:47 -0300

    feat: Adição dos construtores, get e setters da classe Departamento.

--- POOTrabIndividual/src/br/com/poo/Empresa/entities/Departamento.java
+++ POOTrabIndividual/src/br/com/poo/Empresa/entities/Departamento.java
@@ -4,4 +4,43 @@ public class Departamento {
 	private int id;
 	private String nome;
 	private String telefone;
+	
+	public Departamento() {
+		
+	}
+	
+	public Departamento(int id, String nome, String telefone) {
+
+		this.id = id;
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public String toString() {
+		return "ID Departamento: \t\t" + id + "\nNome: \t\t" + nome + "\nTelefone: \t\t" + telefone;
+	}
+	
+	
+	
 }
