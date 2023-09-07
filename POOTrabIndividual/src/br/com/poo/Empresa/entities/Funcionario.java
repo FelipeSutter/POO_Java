4d487ac76e26e45681d9391853b0a42de45f016e
Felipe Sutter <lipeps61@gmail.com>
2023-09-07 19:19:42 -0300

    feat: Adição de construtores, get e set da classe Funcionario

--- POOTrabIndividual/src/br/com/poo/Empresa/entities/Funcionario.java
+++ POOTrabIndividual/src/br/com/poo/Empresa/entities/Funcionario.java
@@ -6,4 +6,49 @@ public class Funcionario {
 	private String cpf;
 	private String telefone;
 	private String dataNasc;
+	
+	public Funcionario() {
+		
+	}
+	
+	public Funcionario(int id, String nome, String cpf, String telefone, String dataNasc) {
+		
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.dataNasc = dataNasc;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getDataNasc() {
+		return dataNasc;
+	}
+
+	@Override
+	public String toString() {
+		return "ID Funcionário: \t\t" + id + "\nNome: \t\t" + nome + "\nCPF: \t\t" + cpf + "\nTelefone: \t\t" + telefone + "\nData de Nascimento: \t\t" + dataNasc;
+	}
+	
+	
+	
 }
