57aaca6e90f7d9f879081d04e120f33e1cb9bb3f
Felipe Sutter <lipeps61@gmail.com>
2023-09-07 19:11:23 -0300

    feat: Adição dos atributos nas classes Funcionario e Departamento

--- POOTrabIndividual/src/br/com/poo/Empresa/entities/Departamento.java
+++ POOTrabIndividual/src/br/com/poo/Empresa/entities/Departamento.java
@@ -1,5 +1,7 @@
 package br.com.poo.Empresa.entities;
 
 public class Departamento {
-
+	private int id;
+	private String nome;
+	private String telefone;
 }
--- POOTrabIndividual/src/br/com/poo/Empresa/entities/Funcionario.java
+++ POOTrabIndividual/src/br/com/poo/Empresa/entities/Funcionario.java
@@ -1,5 +1,9 @@
 package br.com.poo.Empresa.entities;
 
 public class Funcionario {
-
+	private int id;
+	private String nome;
+	private String cpf;
+	private String telefone;
+	private String dataNasc;
 }
